89b03bcbe5a55ea9f452532bf4cabe1e13aff80b
Lokik <kotlin4geek@gmail.com>
2021-02-28 01:40:02 +0530

    Core module integrated, gold theme added and apply in manifest, colors added for gold theme, strings added.

--- app/src/main/java/com/app/batteryalarmgold/MainActivity.kt
+++ app/src/main/java/com/app/batteryalarmgold/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.app.batteryalarmgold
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.app.core.activity.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
